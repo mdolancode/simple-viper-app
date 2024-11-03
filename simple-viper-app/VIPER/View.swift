174a099c9cfa5274489a4801787a6047e126c0fe
Matthew Dolan <mdolancode@gmail.com>
2024-11-03 05:59:57 -0800

    Setup TableView.

--- simple-viper-app/VIPER/View.swift
+++ simple-viper-app/VIPER/View.swift
@@ -21,11 +21,22 @@ protocol AnyView {
     
 }
 
-class UserViewController: UIViewController, AnyView {
+class UserViewController: UIViewController, AnyView, UITableViewDelegate, UITableViewDataSource {
+    
     var presenter: AnyPresenter?
+    
+    private let  tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.isHidden = true
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func update(with users: [User]) {
@@ -34,5 +45,15 @@ class UserViewController: UIViewController, AnyView {
     
     func update(with error: String) {
         
+    }
+    
+    // TableView Delegate and DataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }

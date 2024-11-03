e5fc16f13b712b956302e1200a4866adc26ef42a
Matthew Dolan <mdolancode@gmail.com>
2024-11-03 06:24:56 -0800

    Setup the router and entry point

--- simple-viper-app/VIPER/Router.swift
+++ simple-viper-app/VIPER/Router.swift
@@ -6,19 +6,34 @@
 //
 
 import Foundation
+import UIKit
 
 // Object
 // Entry point
 
+typealias EntryPoint = AnyView & UIViewController
+
 protocol AnyRouter {
+    var entry: EntryPoint? { get }
     static func start() -> AnyRouter
 }
 
 class UserRouter: AnyRouter {
+    var entry: EntryPoint?
     static func start() -> AnyRouter {
         let router = UserRouter()
         
         // Assign VIP
+        var view: AnyView = UserViewController()
+        var presenter: AnyPresenter = UserPresenter()
+        var interactor: AnyInteractor = UserInteractor()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        
+        router.entry = view as? EntryPoint
         
         return router
     }

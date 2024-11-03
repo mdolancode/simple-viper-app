63cde388c67c47c6170b5a6da65546f3ab4355c5
Matthew Dolan <mdolancode@gmail.com>
2024-11-03 05:38:21 -0800

    Setup view protocol.

--- simple-viper-app/VIPER/View.swift
+++ simple-viper-app/VIPER/View.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+// Responsible for user interface
+// View Controller
+// protocol
+// Reference to the presenter
+
+protocol AnyView {
+    var presenter: AnyPresenter? { get set }
+    
+    func update(with users: [User])
+    func update(with error: String)
+    
+}
+
+class UserViewController: UIViewController, AnyView {
+    var presenter: AnyPresenter?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func update(with users: [User]) {
+        
+    }
+    
+    func update(with error: String) {
+        
+    }
+}

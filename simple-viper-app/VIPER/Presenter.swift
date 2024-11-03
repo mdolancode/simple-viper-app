8a0e1fd33790ca38799e50627fbbaea9dd9d161f
Matthew Dolan <mdolancode@gmail.com>
2024-11-03 05:32:59 -0800

    Setup presenter protocol.

--- simple-viper-app/VIPER/Presenter.swift
+++ simple-viper-app/VIPER/Presenter.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+// Object
+// protocol
+// Reference to Interactor, Router, View
+
+protocol AnyPresenter {
+    var router: AnyRouter? { get set }
+    var interactor: AnyInteractor? { get set }
+    var view: AnyView? { get set }
+    
+    func interactorDidFetchUsers(with result: Result<[User], Error>)
+    
+}
+
+class UserPresenter: AnyPresenter {
+    var router: AnyRouter?
+    
+    var interactor: AnyInteractor?
+    
+    var view: AnyView?
+    
+    func interactorDidFetchUsers(with result: Result<[User], Error>) {
+        
+    }
+}
